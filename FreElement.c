4982ce2ee31d0f9717660cd45294e78398e22b85
Moriarty <tphong610198@gmail.com>
2023-10-28 13:20:26 +0700

    Test file .md

--- FreElement.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-#include<math.h>
-#include<stdlib.h>
-#include<stdbool.h>
-int i, j;
-int main(){
-    
-}
---
+++ FreIndecreaseNum.c
@@ -0,0 +1,41 @@
+// #include <stdio.h>
+// #include <string.h>
+// #include <math.h>
+// #include <stdlib.h>
+// bool checkIndecreaseNUm(long long n){
+//     while(n>10){
+//         long long tempt=n%10;
+//         if (tempt < (n/10)%10)
+//             return false;
+//         else{
+//             n= n/10;
+//             return true;       
+//         }
+//     }
+// }
+// void frequence(long long arr[], int n){   
+//     for(int i=0; i<n; i++){
+//         int count = 0 ;
+//         for (int j=0; j<n; j++){
+//             if (arr[i] == arr[j])
+//                 count++;
+//             else
+//                 printf("%d %d ; ", arr[i], count);
+//     }
+// }
+// int main(){
+//     long long arr[] = {1,1,1,1,2,2,2,3,3,3,4,4,4,5,6,6,7,7,7} ;
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     frequence(arr,n);
+// }
+
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+#include<stdbool.h>
+int i, j;
+int main(){
+    char str[100][100];
+    
+}
